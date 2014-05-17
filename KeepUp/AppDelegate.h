c7cf22b60d05d42dd460939d23dfd4facd4c4b27
James Corwin <Obama@Jamess-MacBook.local>
2014-05-17 15:16:22 -0700

    Initial Commit

---
+++ KeepUp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  KeepUp
+//
+//  Created by James Corwin on 5/17/14.
+//  Copyright (c) 2014 James Is a Baller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ KeepUp/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  KeepUp
+//
+
+//  Copyright (c) 2014 James Is a Baller. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
